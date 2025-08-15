3e2cb43aa3bac13059aa0939226e238bf58e48e5
Daan Schouten <173400@student.talland.nl>
2025-08-15 10:02:57 -0700

    test class

---
+++ src/OtpController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Daan0101\LaravelOtpKit;
+
+use Illuminate\Support\Facades\Log;
+
+class OtpController
+{
+    public function test()
+    {
+        Log::info('Test function in OtpController called');
+    }
+}
