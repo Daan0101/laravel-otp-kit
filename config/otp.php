95bba54b588aeb93107ee3fe2280f070672bc3ca
Daan Schouten <173400@student.talland.nl>
2025-08-15 11:28:10 -0700

    refactor codebase

---
+++ config/otp.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel OTP Kit
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the settings for the Laravel OTP Kit.
+    | You can enable or disable the OTP functionality, as well as customize
+    | various aspects of its behavior.
+    |
+    */
+];
