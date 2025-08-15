74c98030f0f24222b95c619d810dfc59fd864872
Daan Schouten <173400@student.talland.nl>
2025-08-15 10:21:40 -0700

    empty provider for now

---
+++ src/Providers/OtpProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Daan0101\LaravelOtpKit\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class OtpProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
