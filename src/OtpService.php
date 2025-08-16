fbe34040a894148f3b7e15d7b23835d44b11c713
Daan Schouten <173400@student.talland.nl>
2025-08-16 04:19:04 -0700

    added migration

--- src/OtpService.php
+++ src/OtpService.php
@@ -11,9 +11,13 @@ class OtpService
         \Log::info('Testing OTP Service');
     }
 
-    public static function generate() {
+    public static function generate($userId): string
+    {
         $length = config('otp.length', 6);
         $OtpCode = Str::random($length);
-        \Log::info('Generated Otp: ' . $OtpCode . ' with length: ' . $length);
+
+        \Log::info('Generated Otp: ' . $OtpCode . ' with length: ' . $length . ' for user: ' . $userId);
+
+        return $OtpCode;
     }
 }
