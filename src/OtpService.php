7c896ac97dac3215debcb9b9a869ff631c7803b6
Daan Schouten <173400@student.talland.nl>
2025-08-16 03:52:45 -0700

    feat: generate otp code

--- src/OtpService.php
+++ src/OtpService.php
@@ -3,10 +3,17 @@
 namespace Daanp\LaravelOtpKit;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class OtpService
 {
     public static function test() {
         \Log::info('Testing OTP Service');
+    }
+
+    public static function generate() {
+        $length = config('otp.length', 6);
+        $OtpCode = Str::random($length);
+        \Log::info('Generated Otp: ' . $OtpCode . ' with length: ' . $length);
     }
 }
